18f55c9f46f5da5fca42f3f3a549c3470f5c72ae
guidejet <newnewernewest123@gmail.com>
2025-03-13 20:25:00 +0500

    issues: position name and deprtment name takes as array

--- src/views/pages/department/department.vue
+++ src/views/pages/department/department.vue
@@ -18,11 +18,11 @@ const {
         <!-- <img src="https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D" alt="Profile" style="width:100%"> -->
         <img :src="person.image" alt="">
         <h1>{{ person.first_name }} {{ person.last_name }}</h1>
-        <p class="title">{{ person.position }}</p>
-        <p>{{ person.department }}</p>
-        <div class="projcard-tagbox">
+        <p class="title" v-for="(position_name,index) in department.position" :key="index">{{ position_name.position }}</p>
+        <p v-for="(department_name, index) in department.department" :key="index">{{ department_name.department }}</p>
+        <!-- <div class="projcard-tagbox">
 			<a v-for="(tag, index) in newss.tags" :key="index" href="#" class="projcard-tag">{{ tag.name }}</a>
-			</div>
+			</div> -->
         <div style="display: flex; justify-content: center;">
           <a v-if="person.telegram_id" style="padding: 10px;" :href="`https://t.me/${person.telegram_id}`"><i class="fa fa-telegram"></i></a>
           <a v-if="person.phone_number" style="padding: 10px;" :href="`https://wa.me/${person.phone_number}`"><i class="fa fa-whatsapp"></i></a>
