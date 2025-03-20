02fd770856ed7029c66383b08b74d39cf3774d1c
guidejet <newnewernewest123@gmail.com>
2025-03-20 20:34:07 +0500

    m

--- src/main.js
+++ src/main.js
@@ -6,20 +6,20 @@ import '@/assets/css/main.css'
 import Antd from 'ant-design-vue';
 import { AntDesignOutlined } from "@ant-design/icons-vue";
 import { createPinia } from 'pinia';
-import axios from "axios";
-// Функция для получения CSRF-токена из куков
-function getCsrfToken() {
-    return document.cookie
-        .split('; ')
-        .find(row => row.startsWith('csrftoken='))
-        ?.split('=')[1];
-}
+// import axios from "axios";
+// // Функция для получения CSRF-токена из куков
+// function getCsrfToken() {
+//     return document.cookie
+//         .split('; ')
+//         .find(row => row.startsWith('csrftoken='))
+//         ?.split('=')[1];
+// }
 
-// Добавляем CSRF-токен во все POST/PUT/PATCH/DELETE-запросы
-axios.defaults.headers.common['X-CSRFToken'] = getCsrfToken();
+// // Добавляем CSRF-токен во все POST/PUT/PATCH/DELETE-запросы
+// axios.defaults.headers.common['X-CSRFToken'] = getCsrfToken();
 
-// Если используешь сессии, включи withCredentials
-axios.defaults.withCredentials = true;
+// // Если используешь сессии, включи withCredentials
+// axios.defaults.withCredentials = true;
 const app = createApp(App)
 const pinia = createPinia();
 app.use(pinia);
