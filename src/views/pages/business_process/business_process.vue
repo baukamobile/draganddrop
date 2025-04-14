7f716736e55cf4f5a859f6426444b0a128753d43
guidejet <newnewernewest123@gmail.com>
2025-04-14 17:35:54 +0500

    --Added bpmn template

--- src/views/pages/business_process/business_process.vue
+++ src/views/pages/business_process/business_process.vue
@@ -1,4 +1,4 @@
-<script setup>
+<!-- <script setup>
 import { ref, watch, reactive } from "vue";
 import { useTaskManager } from "./BpmManager";
 import './style/style.css';
@@ -57,12 +57,12 @@ const selectedProjectId = ref(Number(route.params.process_id));
                 <div class="center">
                     <div v-for="status in process_stages" :key="status.id" @drop="onDrop($event, status.id)" class="droppable"
     @dragover.prevent @dragenter.prevent>
-                        <!-- Разрешение для перетаскивание -->
-                        <div class="status">
+                        Разрешение для перетаскивание -->
+                        <!-- <div class="status"> 
                             <h1 class="status-name">{{ status.name }}</h1>
-                            <!-- <a-button class="red-button" @click="handleClick(status.id)">Удалить колонку -->
+                            <a-button class="red-button" @click="handleClick(status.id)">Удалить колонку 
                             <a class="red-button" @click="handleClick(status.id)">
-                                <!-- <DeleteOutlined style="font-size: 15px; color: red; cursor: pointer;" /> -->
+                                 <DeleteOutlined style="font-size: 15px; color: red; cursor: pointer;" /> 
                                 <el-icon style="font-size: 15px; color: red; cursor: pointer;"><DeleteFilled /></el-icon>
                             </a>
                         </div>
@@ -73,9 +73,9 @@ const selectedProjectId = ref(Number(route.params.process_id));
                                 <div class="form1">
                                     <p style="margin: 0;">
                                         {{ task.title }}
-                                        <!-- <span :style="{ color: priority[task.priority]?.color }">
+                                        <span :style="{ color: priority[task.priority]?.color }">
                                             ({{ priority[task.priority]?.priority_name || 'Неизвестный' }})
-                                        </span> -->
+                                        </span> 
                                     </p>
                                     <div style="display: flex;">
                                         <a @click.prevent="handleClickTask(task.id)" class="delete-task">
@@ -97,17 +97,17 @@ const selectedProjectId = ref(Number(route.params.process_id));
                             </div>
                         </transition-group>
                         <!-- Здсь форма для добавление задач с вводимым данными -->
-                        <div class="add-task-container">
+                        <!-- <div class="add-task-container"> 
                             <a @click="toggleTaskForm(status.id)" href="#" class="add-task">Добавить Задачу</a>
-                            <!-- порма открывается только выбранного колонки с помошью id -->
-                            <div v-if="showTaskForm[status.id]">
-                                <!-- <a @click="showTaskForm = !showTaskForm" href="#" class="add-task">Скрыть</a> -->
-                                <h2>Форма</h2>
+                             порма открывается только выбранного колонки с помошью id -->
+                            <!-- <div v-if="showTaskForm[status.id]"> 
+                                <a @click="showTaskForm = !showTaskForm" href="#" class="add-task">Скрыть</a> -->
+                                <!-- <h2>Форма</h2>
                                 <form @submit.prevent="newTask.id ? updateTask() : submitTask();">
 
                                     <label class="label-name">Название:</label>
                                     <input v-model="newTask.title" placeholder="Название задачи" required>>
-                                    <!-- <input v-model="newTask.task_name" placeholder="Название задачи" required> -->
+                                    <input v-model="newTask.task_name" placeholder="Название задачи" required> 
                                     <br><br>
                                     <label class="label-name">Описание:</label>
                                     <input v-model="newTask.description" placeholder="Описание задачи">
@@ -123,8 +123,8 @@ const selectedProjectId = ref(Number(route.params.process_id));
                                     <input type="date" v-model="newTask.due_date">
                                     <!-- @input="newTask.end_date = $event.target.value || null"> -->
                                     <!-- <br><br> -->
-                                    <!-- <label>Отдел</label>
-                                    <select v-model="newTask.department"
+                                    <!-- <label>Отдел</label> -->
+                                    <!-- <select v-model="newTask.department" 
                                         @change="console.log('отдел: ', newTask.department)">
                                         <option v-for="dep in department" :key="dep.id" :value="dep.id">
                                             {{ dep.department_name }}
@@ -132,9 +132,9 @@ const selectedProjectId = ref(Number(route.params.process_id));
                                     </select> -->
                                     <!-- <label class="label-name">Согласовано с руководством:</label>
                                     <input type="checkbox" v-model="newTask.agreed_with_managers"> -->
-                                    <br><br>
-                                    <label class="label-name">Подписан с :</label>
-                                    <!-- <label class="label-name">Название проекта:</label> -->
+                                    <!-- <br><br> -->
+                                    <!-- <label class="label-name">Подписан с :</label> -->
+                                    <!-- <label class="label-name">Название проекта:</label> 
                                     <select id="users" v-model="newTask.created_by">
                                         <option v-for="user in users" :key="user.id" :value="Number(user.id)">
                                             {{ user.first_name }}
@@ -152,33 +152,33 @@ const selectedProjectId = ref(Number(route.params.process_id));
                                         <option v-for="(priority_value, priority_key) in priority" :key="priority_key"
                                             :value="priority_key">{{ priority_value.priority_name }}</option>
                                     </select> -->
-                                    <br><br>
-                                    <!-- <select v-model="newTask.status">
+                                    <!-- <br><br>
+                                    <select v-model="newTask.status">
                                         <option v-for="status in statuses" :key="status.id" :value="status.id">
                                             {{ status.status_name }}</option>
                                     </select> -->
 
                                     <!-- <a href="#">Добавить</a> -->
-                                    <button type="submit ">Добавить</button>
+                                    <!-- <button type="submit ">Добавить</button>
                                 </form>
                             </div>
                         </div>
                     </div>
                     <div class="add-list">
-                        <!--  форма для доавление колонок -->
-                        <h2>Добавить колонку</h2>
+                          форма для доавление колонок -->
+                        <!-- <h2>Добавить колонку</h2>
                         <form @submit.prevent="submitColumn">
                             <input v-model="newProcessStage.name" placeholder="Название колонки" required />
                             <div><label for="users">Сотрудник:</label><br>
                                 <select id="users" v-model="newProcessStage.owner">
                                     <option v-for="owners in newProcessStage.owner" :key="owners.id" :value="Number(owners.id)">
                                         {{ owners.first_name }} 
-                                        <!-- {{ user.last_name }} -->
+                                        {{ user.last_name }} -->
                                         <!-- {{ user.department.department_name }} -->
-                                        <!-- {{ user.department ? user.department.department_name : 'No department' }} -->
+                                        <!-- {{ user.department ? user.department.department_name : 'No department' }} 
                                     </option>
-                                </select>
-                            </div>
+                                </select> -->
+                            <!-- </div>
                             <button type="submit">Добавить</button>
                         </form>
                     </div>
@@ -186,7 +186,160 @@ const selectedProjectId = ref(Number(route.params.process_id));
                 </div>
 
             </div>
-        </div>
+        </div> -->
         <!-- </div> -->
-    </PageWrapper>
+    <!-- </PageWrapper> 
+</template> --> 
+<script setup>
+import { onMounted, ref } from 'vue';
+import BpmnModeler from 'bpmn-js/lib/Modeler';
+import 'bpmn-js/dist/assets/diagram-js.css';
+import 'bpmn-js/dist/assets/bpmn-font/css/bpmn.css';
+
+const bpmnContainer = ref(null);
+
+onMounted(() => {
+  const modeler = new BpmnModeler({
+    container: bpmnContainer.value,
+    keyboard: { bindTo: document },
+  });
+
+  const initialDiagram = `<?xml version="1.0" encoding="UTF-8"?>
+    <bpmn:definitions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                      xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL"
+                      xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI"
+                      xmlns:dc="http://www.omg.org/spec/DD/20100524/DC"
+                      id="Definitions_1"
+                      targetNamespace="http://bpmn.io/schema/bpmn">
+      <bpmn:process id="Process_1" isExecutable="true">
+        <bpmn:startEvent id="StartEvent_1">
+          <bpmn:outgoing>Flow_1</bpmn:outgoing>
+        </bpmn:startEvent>
+        <bpmn:task id="Task_1" name="Выполнить задачу">
+          <bpmn:incoming>Flow_1</bpmn:incoming>
+          <bpmn:outgoing>Flow_2</bpmn:outgoing>
+        </bpmn:task>
+        <bpmn:parallelGateway id="ParallelGateway_1">
+          <bpmn:incoming>Flow_2</bpmn:incoming>
+          <bpmn:outgoing>Flow_3</bpmn:outgoing>
+        </bpmn:parallelGateway>
+        <bpmn:endEvent id="EndEvent_1">
+          <bpmn:incoming>Flow_3</bpmn:incoming>
+        </bpmn:endEvent>
+        <bpmn:sequenceFlow id="Flow_1" sourceRef="StartEvent_1" targetRef="Task_1"/>
+        <bpmn:sequenceFlow id="Flow_2" sourceRef="Task_1" targetRef="ParallelGateway_1"/>
+        <bpmn:sequenceFlow id="Flow_3" sourceRef="ParallelGateway_1" targetRef="EndEvent_1"/>
+      </bpmn:process>
+      <bpmndi:BPMNDiagram id="BPMNDiagram_1">
+        <bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="Process_1">
+          <bpmndi:BPMNShape id="StartEvent_1_di" bpmnElement="StartEvent_1">
+            <dc:Bounds x="800" y="100" width="36" height="36"/>
+          </bpmndi:BPMNShape>
+          <bpmndi:BPMNShape id="Task_1_di" bpmnElement="Task_1">
+            <dc:Bounds x="900" y="80" width="100" height="80"/>
+          </bpmndi:BPMNShape>
+          <bpmndi:BPMNShape id="ParallelGateway_1_di" bpmnElement="ParallelGateway_1">
+            <dc:Bounds x="1050" y="100" width="50" height="50"/>
+          </bpmndi:BPMNShape>
+          <bpmndi:BPMNShape id="EndEvent_1_di" bpmnElement="EndEvent_1">
+            <dc:Bounds x="1150" y="100" width="36" height="36"/>
+          </bpmndi:BPMNShape>
+
+          <bpmndi:BPMNEdge id="Flow_1_di" bpmnElement="Flow_1">
+            <dc:waypoint x="836" y="118"/>
+            <dc:waypoint x="900" y="118"/>
+          </bpmndi:BPMNEdge>
+          <bpmndi:BPMNEdge id="Flow_2_di" bpmnElement="Flow_2">
+            <dc:waypoint x="1000" y="118"/>
+            <dc:waypoint x="1050" y="118"/>
+          </bpmndi:BPMNEdge>
+          <bpmndi:BPMNEdge id="Flow_3_di" bpmnElement="Flow_3">
+            <dc:waypoint x="1100" y="118"/>
+            <dc:waypoint x="1150" y="118"/>
+          </bpmndi:BPMNEdge>
+          <bpmndi:BPMNShape id="ParallelGateway_1_di" bpmnElement="ParallelGateway_1">
+  <dc:Bounds x="950" y="50" width="50" height="50"/>
+</bpmndi:BPMNShape>
+        </bpmndi:BPMNPlane>
+      </bpmndi:BPMNDiagram>
+    </bpmn:definitions>`;
+
+  modeler.importXML(initialDiagram).then(() => {
+    console.log('BPMN загружен!');
+    modeler.get('canvas').zoom('fit-viewport');
+  }).catch(err => {
+    console.error('Ошибка загрузки BPMN:', err);
+  });
+
+  // const saveDiagram = async () => {
+  //   try {
+  //     const { xml } = await modeler.saveXML({ format: true });
+  //     console.log('Сохранённый XML:', xml);
+  //     // Для backend: await fetch('/api/processes/', { method: 'POST', body: JSON.stringify({ xml }) });
+  //   } catch (err) {
+  //     console.error('Ошибка сохранения:', err);
+  //   }
+  // };
+  const saveDiagram = async () => {
+  try {
+    const { xml } = await modeler.saveXML({ format: true });
+    console.log('Сохранённый XML:', xml);
+
+    // Отправка на сервер
+    const response = await fetch('127.0.0.1:8000/bpm/xml-processes/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ xml }),
+    });
+
+    if (!response.ok) {
+      throw new Error('Ошибка отправки на сервер');
+    }
+
+    console.log('Диаграмма успешно сохранена на сервере');
+  } catch (err) {
+    console.error('Ошибка сохранения:', err);
+  }
+};
+
+  return { saveDiagram };
+});
+</script>
+
+<template>
+  <div class="flex flex-col h-full" style="display: flex; justify-content: space-between;">
+    <div ref="bpmnContainer" class="w-full h-[700px] border" >
+    <button @click="saveDiagram" class="mt-2 p-2 bg-blue-500 text-white self-start">
+      Сохранить процесс
+    </button>
+    <button @click="saveDiagram" class="mt-2 p-2 bg-green-500 text-white self-start">
+      Запустить процесс
+    </button>
+</div>
+  </div>
 </template>
+
+<style scoped>
+/* Фикс для редактора */
+:deep(.bjs-container) {
+  height: 100%;
+  width: 100%;
+}
+
+/* Стили для кнопки */
+button {
+  border-radius: 4px;
+}
+
+/* Контейнер */
+.w-full {
+  background-color: rgb(234, 234, 235);
+}
+
+/* Убираем кривые стили */
+:deep(.djs-hit) {
+  
+}
+</style>
--- src/views/pages/reports/reports.vue
+++ src/views/pages/reports/reports.vue
@@ -1,347 +1,149 @@
-<script lang="ts" setup>
-import PageWrapper from '@/components/PageWrapper.vue'
-import StatisticsSection from '@/components/pages/dashboard/StatisticsSection.vue'
-import SalesSection from '@/components/pages/dashboard/SalesSection.vue'
-import LatestSection from '@/components/pages/dashboard/LatestSection.vue'
-import Button from '@/components/Button.vue'
-import { ref } from 'vue'
+<script setup>
+import { onMounted, ref } from 'vue';
+import BpmnModeler from 'bpmn-js/lib/Modeler';
+import 'bpmn-js/dist/assets/diagram-js.css';
+import 'bpmn-js/dist/assets/bpmn-font/css/bpmn.css';
 
-const value = ref([])
+const bpmnContainer = ref(null);
 
-const props = {
-  expandTrigger: 'hover' as const,
-}
+onMounted(() => {
+  const modeler = new BpmnModeler({
+    container: bpmnContainer.value,
+    keyboard: { bindTo: document },
+  });
 
-const handleChange = (value) => {
-  console.log(value)
-}
+  const initialDiagram = `<?xml version="1.0" encoding="UTF-8"?>
+    <bpmn:definitions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                      xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL"
+                      xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI"
+                      xmlns:dc="http://www.omg.org/spec/DD/20100524/DC"
+                      id="Definitions_1"
+                      targetNamespace="http://bpmn.io/schema/bpmn">
+      <bpmn:process id="Process_1" isExecutable="true">
+        <bpmn:startEvent id="StartEvent_1">
+          <bpmn:outgoing>Flow_1</bpmn:outgoing>
+        </bpmn:startEvent>
+        <bpmn:task id="Task_1" name="Выполнить задачу">
+          <bpmn:incoming>Flow_1</bpmn:incoming>
+          <bpmn:outgoing>Flow_2</bpmn:outgoing>
+        </bpmn:task>
+        <bpmn:parallelGateway id="ParallelGateway_1">
+          <bpmn:incoming>Flow_2</bpmn:incoming>
+          <bpmn:outgoing>Flow_3</bpmn:outgoing>
+        </bpmn:parallelGateway>
+        <bpmn:endEvent id="EndEvent_1">
+          <bpmn:incoming>Flow_3</bpmn:incoming>
+        </bpmn:endEvent>
+        <bpmn:sequenceFlow id="Flow_1" sourceRef="StartEvent_1" targetRef="Task_1"/>
+        <bpmn:sequenceFlow id="Flow_2" sourceRef="Task_1" targetRef="ParallelGateway_1"/>
+        <bpmn:sequenceFlow id="Flow_3" sourceRef="ParallelGateway_1" targetRef="EndEvent_1"/>
+      </bpmn:process>
+      <bpmndi:BPMNDiagram id="BPMNDiagram_1">
+        <bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="Process_1">
+          <bpmndi:BPMNShape id="StartEvent_1_di" bpmnElement="StartEvent_1">
+            <dc:Bounds x="800" y="100" width="36" height="36"/>
+          </bpmndi:BPMNShape>
+          <bpmndi:BPMNShape id="Task_1_di" bpmnElement="Task_1">
+            <dc:Bounds x="900" y="80" width="100" height="80"/>
+          </bpmndi:BPMNShape>
+          <bpmndi:BPMNShape id="ParallelGateway_1_di" bpmnElement="ParallelGateway_1">
+            <dc:Bounds x="1050" y="100" width="50" height="50"/>
+          </bpmndi:BPMNShape>
+          <bpmndi:BPMNShape id="EndEvent_1_di" bpmnElement="EndEvent_1">
+            <dc:Bounds x="1150" y="100" width="36" height="36"/>
+          </bpmndi:BPMNShape>
 
-const options = [
-  {
-    value: 'guide',
-    label: 'Guide',
-    children: [
-      {
-        value: 'disciplines',
-        label: 'Disciplines',
-        children: [
-          {
-            value: 'consistency',
-            label: 'Consistency',
-          },
-          {
-            value: 'feedback',
-            label: 'Feedback',
-          },
-          {
-            value: 'efficiency',
-            label: 'Efficiency',
-          },
-          {
-            value: 'controllability',
-            label: 'Controllability',
-          },
-        ],
+          <bpmndi:BPMNEdge id="Flow_1_di" bpmnElement="Flow_1">
+            <dc:waypoint x="836" y="118"/>
+            <dc:waypoint x="900" y="118"/>
+          </bpmndi:BPMNEdge>
+          <bpmndi:BPMNEdge id="Flow_2_di" bpmnElement="Flow_2">
+            <dc:waypoint x="1000" y="118"/>
+            <dc:waypoint x="1050" y="118"/>
+          </bpmndi:BPMNEdge>
+          <bpmndi:BPMNEdge id="Flow_3_di" bpmnElement="Flow_3">
+            <dc:waypoint x="1100" y="118"/>
+            <dc:waypoint x="1150" y="118"/>
+          </bpmndi:BPMNEdge>
+          <bpmndi:BPMNShape id="ParallelGateway_1_di" bpmnElement="ParallelGateway_1">
+  <dc:Bounds x="950" y="50" width="50" height="50"/>
+</bpmndi:BPMNShape>
+        </bpmndi:BPMNPlane>
+      </bpmndi:BPMNDiagram>
+    </bpmn:definitions>`;
+
+  modeler.importXML(initialDiagram).then(() => {
+    console.log('BPMN загружен!');
+    modeler.get('canvas').zoom('fit-viewport');
+  }).catch(err => {
+    console.error('Ошибка загрузки BPMN:', err);
+  });
+
+  // const saveDiagram = async () => {
+  //   try {
+  //     const { xml } = await modeler.saveXML({ format: true });
+  //     console.log('Сохранённый XML:', xml);
+  //     // Для backend: await fetch('/api/processes/', { method: 'POST', body: JSON.stringify({ xml }) });
+  //   } catch (err) {
+  //     console.error('Ошибка сохранения:', err);
+  //   }
+  // };
+  const saveDiagram = async () => {
+  try {
+    const { xml } = await modeler.saveXML({ format: true });
+    console.log('Сохранённый XML:', xml);
+
+    // Отправка на сервер
+    const response = await fetch('127.0.0.1:8000/bpm/xml-processes/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
       },
-      {
-        value: 'navigation',
-        label: 'Navigation',
-        children: [
-          {
-            value: 'side nav',
-            label: 'Side Navigation',
-          },
-          {
-            value: 'top nav',
-            label: 'Top Navigation',
-          },
-        ],
-      },
-    ],
-  },
-  {
-    value: 'component',
-    label: 'Component',
-    children: [
-      {
-        value: 'basic',
-        label: 'Basic',
-        children: [
-          {
-            value: 'layout',
-            label: 'Layout',
-          },
-          {
-            value: 'color',
-            label: 'Color',
-          },
-          {
-            value: 'typography',
-            label: 'Typography',
-          },
-          {
-            value: 'icon',
-            label: 'Icon',
-          },
-          {
-            value: 'button',
-            label: 'Button',
-          },
-        ],
-      },
-      {
-        value: 'form',
-        label: 'Form',
-        children: [
-          {
-            value: 'radio',
-            label: 'Radio',
-          },
-          {
-            value: 'checkbox',
-            label: 'Checkbox',
-          },
-          {
-            value: 'input',
-            label: 'Input',
-          },
-          {
-            value: 'input-number',
-            label: 'InputNumber',
-          },
-          {
-            value: 'select',
-            label: 'Select',
-          },
-          {
-            value: 'cascader',
-            label: 'Cascader',
-          },
-          {
-            value: 'switch',
-            label: 'Switch',
-          },
-          {
-            value: 'slider',
-            label: 'Slider',
-          },
-          {
-            value: 'time-picker',
-            label: 'TimePicker',
-          },
-          {
-            value: 'date-picker',
-            label: 'DatePicker',
-          },
-          {
-            value: 'datetime-picker',
-            label: 'DateTimePicker',
-          },
-          {
-            value: 'upload',
-            label: 'Upload',
-          },
-          {
-            value: 'rate',
-            label: 'Rate',
-          },
-          {
-            value: 'form',
-            label: 'Form',
-          },
-        ],
-      },
-      {
-        value: 'data',
-        label: 'Data',
-        children: [
-          {
-            value: 'table',
-            label: 'Table',
-          },
-          {
-            value: 'tag',
-            label: 'Tag',
-          },
-          {
-            value: 'progress',
-            label: 'Progress',
-          },
-          {
-            value: 'tree',
-            label: 'Tree',
-          },
-          {
-            value: 'pagination',
-            label: 'Pagination',
-          },
-          {
-            value: 'badge',
-            label: 'Badge',
-          },
-        ],
-      },
-      {
-        value: 'notice',
-        label: 'Notice',
-        children: [
-          {
-            value: 'alert',
-            label: 'Alert',
-          },
-          {
-            value: 'loading',
-            label: 'Loading',
-          },
-          {
-            value: 'message',
-            label: 'Message',
-          },
-          {
-            value: 'message-box',
-            label: 'MessageBox',
-          },
-          {
-            value: 'notification',
-            label: 'Notification',
-          },
-        ],
-      },
-      {
-        value: 'navigation',
-        label: 'Navigation',
-        children: [
-          {
-            value: 'menu',
-            label: 'Menu',
-          },
-          {
-            value: 'tabs',
-            label: 'Tabs',
-          },
-          {
-            value: 'breadcrumb',
-            label: 'Breadcrumb',
-          },
-          {
-            value: 'dropdown',
-            label: 'Dropdown',
-          },
-          {
-            value: 'steps',
-            label: 'Steps',
-          },
-        ],
-      },
-      {
-        value: 'others',
-        label: 'Others',
-        children: [
-          {
-            value: 'dialog',
-            label: 'Dialog',
-          },
-          {
-            value: 'tooltip',
-            label: 'Tooltip',
-          },
-          {
-            value: 'popover',
-            label: 'Popover',
-          },
-          {
-            value: 'card',
-            label: 'Card',
-          },
-          {
-            value: 'carousel',
-            label: 'Carousel',
-          },
-          {
-            value: 'collapse',
-            label: 'Collapse',
-          },
-        ],
-      },
-    ],
-  },
-  {
-    value: 'resource',
-    label: 'Resource',
-    children: [
-      {
-        value: 'axure',
-        label: 'Axure Components',
-      },
-      {
-        value: 'sketch',
-        label: 'Sketch Templates',
-      },
-      {
-        value: 'docs',
-        label: 'Design Documentation',
-      },
-    ],
-  },
-]
-const checkList = ref(['Value selected and disabled', 'Value A'])
-const valueDate = ref('')
-const defaultTime = ref<[Date, Date]>([
-  new Date(2000, 1, 1, 0, 0, 0),
-  new Date(2000, 2, 1, 23, 59, 59),
-])
+      body: JSON.stringify({ xml }),
+    });
+
+    if (!response.ok) {
+      throw new Error('Ошибка отправки на сервер');
+    }
+
+    console.log('Диаграмма успешно сохранена на сервере');
+  } catch (err) {
+    console.error('Ошибка сохранения:', err);
+  }
+};
+
+  return { saveDiagram };
+});
 </script>
 
-
-
 <template>
-    <PageWrapper>
-        <template #header>
-            <div
-                class="flex flex-col gap-4 md:flex-row md:items-center md:justify-between"
-            >
-                <h2 class="text-xl font-semibold leading-tight">Отчеты</h2>
-
-            </div>
-            <div class="m-4">
-      <p>Child options expand when clicked (default)</p>
-      <el-cascader v-model="value" :options="options" @change="handleChange" />
-    </div>
-    <div class="m-4">
-      <p>Child options expand when hovered</p>
-      <el-cascader
-        v-model="value"
-        :options="options"
-        :props="props"
-        @change="handleChange"
-      />
-    </div>
-    <el-checkbox-group v-model="checkList">
-    <el-checkbox label="Option A" value="Value A" />
-    <el-checkbox label="Option B" value="Value B" />
-    <el-checkbox label="Option C" value="Value C" />
-    <el-checkbox label="disabled" value="Value disabled" disabled />
-    <el-checkbox
-      label="selected and disabled"
-      value="Value selected and disabled"
-      disabled
-    />
-  </el-checkbox-group>
-  <div class="demo-date-picker">
-    <div class="block">
-      <p>Component value：{{ value }}</p>
-      <el-date-picker
-        v-model="valueDate"
-        type="daterange"
-        start-placeholder="Start date"
-        end-placeholder="End date"
-        :default-time="defaultTime"
-      />
-    </div>
+  <div class="flex flex-col h-full">
+    <div ref="bpmnContainer" class="w-full h-[700px] border" />
+    <button @click="saveDiagram" class="mt-2 p-2 bg-blue-500 text-white self-start">
+      Сохранить процесс
+    </button>
   </div>
-  <el-button type="primary">Primary</el-button>
-        </template>
-
-   
-    </PageWrapper>
 </template>
+
+<style scoped>
+/* Фикс для редактора */
+:deep(.bjs-container) {
+  height: 100%;
+  width: 100%;
+}
+
+/* Стили для кнопки */
+button {
+  border-radius: 4px;
+}
+
+/* Контейнер */
+.w-full {
+  background-color: rgb(234, 234, 235);
+}
+
+/* Убираем кривые стили */
+:deep(.djs-hit) {
+  /* Ничего не делаем, color не работает для SVG */
+}
+</style>
